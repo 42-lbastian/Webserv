82d446d0be5cc831a6fb6d2744a126d1eb963f6c
lbastian <loic.bastian@gmail.com>
2024-01-03 17:33:26 +0100

    Test add epoll -> fail

--- test/toto.cpp
+++ test/toto.cpp
@@ -9,19 +9,24 @@
 #include <cerrno>
 #include <unistd.h>
 #include <vector>
+#include <sys/epoll.h>
 
 int main(int argc, char *argv[])
 {
 	struct addrinfo hints, *res;
-	int status;
+	struct sockaddr_storage their_addr;
+	struct sockaddr_in *ipv4; 
+	struct epoll_event event;
 	char ipstr[INET6_ADDRSTRLEN];
 	void *addr;
-	struct sockaddr_in *ipv4; 
-	int sock, sock_accept;
-	struct sockaddr_storage their_addr;
 	socklen_t addr_size;
 	const int enable = 1;
 	int size;
+	int status;
+	int sock, sock_accept;
+	int fd_epoll;
+	int epoll_wait_ret;
+	std::vector<char> buff_recv(1);
 
 	if (argc != 2) {
 		fprintf(stderr,"usage: showip hostname\n");
@@ -38,7 +43,7 @@ int main(int argc, char *argv[])
 	if ((status = getaddrinfo(argv[1], "42424", &hints, &res)) != 0) {
 		//gai_strerror translate error
 		std::cerr << "getaddrinfo:" << gai_strerror(status) << std::endl; 
-		return (2);
+		return (1);
 	}
 	
 	std::cout << "IP addresses for " << argv[1] << std::endl << std::endl;
@@ -58,7 +63,7 @@ int main(int argc, char *argv[])
 	if (sock == -1)
 	{
 		std::cerr << "Socket Error: " << strerror(errno) << std::endl;
-		return (3);
+		return (1);
 	}
 
 	//setsockopt: (server socket ; SOL_SOCKET(enable manipulation socket) ; 
@@ -68,7 +73,7 @@ int main(int argc, char *argv[])
 	if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) == -1 && setsockopt(sock, SOL_SOCKET, SO_REUSEPORT, &enable, sizeof(int)) == -1)
 	{
 		std::cerr << "Setsock Error: " << strerror(errno) << std::endl;
-		return (4);
+		return (1);
 	}
 
 	//bind: (server socket ; struct sockaddr with address info ; lenght in bytes of address)
@@ -77,7 +82,7 @@ int main(int argc, char *argv[])
 	if (bind(sock, res->ai_addr, res->ai_addrlen) == -1)
 	{
 		std::cerr << "Bind Error: " << strerror(errno) << std::endl;
-		return (5);
+		return (1);
 	}
 
 	//listen: (server socket ; nb connection allowed in queue)
@@ -85,44 +90,64 @@ int main(int argc, char *argv[])
 	if (listen(sock, 5) == -1)
 	{
 		std::cerr << "Listen Error: " << strerror(errno) << std::endl;
-		return (6);
+		return (1);
 	}
 
 	addr_size = sizeof(their_addr);
-		//accept: (server socket ; storage struct ; size of storage struct)
-			//return -1 - error ; valid fd(client socket) - success
-		sock_accept = accept(sock, (struct sockaddr *)&their_addr, &addr_size);
-		if (sock_accept == -1)
-		{
-			std::cerr << "Accept Error: " << strerror(errno) << std::endl;
-			return (7);
-		}
+	//accept: (server socket ; storage struct ; size of storage struct)
+		//return -1 - error ; valid fd(client socket) - success
+	sock_accept = accept(sock, (struct sockaddr *)&their_addr, &addr_size);
+	if (sock_accept == -1)
+	{
+		std::cerr << "Accept Error: " << strerror(errno) << std::endl;
+		return (1);
+	}
+
+	//epoll_create: (size(ignored but must be greater than one))
+		//return -1 - error ; valid fd - sucess
+	fd_epoll = epoll_create(1);
+	if (fd_epoll == -1)
+	{
+		std::cerr << "Epoll_create error" << std::endl;
+		return (1);
+	}
+
+	event.events = EPOLLIN;
+	event.data.fd = sock_accept;
+
+	if (epoll_ctl(fd_epoll, EPOLL_CTL_ADD, sock_accept, &event) == -1)
+	{
+		std::cerr << "Epoll_ctl error" << std::endl;
+		return (1);
+	}
 
 	while (1)
 	{
-        if (!fork())
-		{ // this is the child process
-            close(sock); // child doesn't need the listener
-			std::vector<char> buff_recv(1);
+		epoll_wait_ret = epoll_wait(fd_epoll, &event, 10, -1);
+		if (epoll_wait_ret == -1)
+		{
+			std::cerr << "Epoll_wait fail" << std::endl;
+			return (1);
+		}
+
+		if (event.data.fd ==  sock_accept)
+		{
 			if ((size = recv(sock_accept, buff_recv.data(), buff_recv.size(), 0)) == -1)
 			{
 				std::cerr << "Recv Error: " << strerror(errno) << std::endl;
-				return (8);
+				return (1);
 			}
-	
+		}
+		else
+		{
 			//send: (client socket, message, length(message), flags)
 				//return -1 - error ; number of byte sent - success
 			if (send(sock_accept, buff_recv.data(), buff_recv.size(), 0) == -1)
 			{
 				std::cerr << "Send Error: " << strerror(errno) << std::endl;
-				return (9);
+				return (1);
 			}
-            close(sock_accept);
-            _exit(0);
-        }
-
-
-
+		}
 	}
 	close(sock_accept);
 	close(sock);
